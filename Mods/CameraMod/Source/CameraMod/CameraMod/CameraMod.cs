1b74d4d6ed500913ea911f6a2c7654c95ce675dd
striker666dk <xxxstriker666xxx@gmail.com>
2016-04-24 20:29:40 +0200

    Structure Update

---
+++ Mods/CameraMod/Source/CameraMod/CameraMod/CameraMod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CameraMod
+{
+    public class CameraMod : PartModule{
+        void Update() {
+            print("CameraMod FTW!!!");
+        }
+    }
+}
